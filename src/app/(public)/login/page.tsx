01b91d4db9928c592bb5c3a597138f478f073249
shubh <tiwarishubhanshu22499@gmail.com>
2026-01-01 12:54:50 +0530

    updated login file

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -5,7 +5,7 @@ import LoginClient from "./LoginClient";
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div className="text-center mt-20">Loading...</div>}>
+    <Suspense fallback={null}>
       <LoginClient />
     </Suspense>
   );
