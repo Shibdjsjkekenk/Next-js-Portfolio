3d19556ea7e45cb78776b770cccc5adf9159d300
shubh <tiwarishubhanshu22499@gmail.com>
2026-01-22 23:33:38 +0530

    updated update order api page

--- src/app/api/timeline/update-order/route.ts
+++ src/app/api/timeline/update-order/route.ts
@@ -3,11 +3,11 @@ import { connectDB } from "@/lib/db";
 import Timeline from "@/models/Timeline";
 import redis from "@/lib/redis";
 import { CACHE_KEYS } from "@/lib/cacheKeys";
+import { revalidatePath } from "next/cache"; // 🔥 ADD THIS
 
 export async function PUT(req: NextRequest) {
   try {
     const { orders } = await req.json();
-    // orders = [{ id, order }]
 
     await connectDB();
 
@@ -20,8 +20,13 @@ export async function PUT(req: NextRequest) {
 
     await Timeline.bulkWrite(bulk);
 
-    //  clear cache
+    // Clear Redis
     await redis.del(CACHE_KEYS.TIMELINE_ALL);
+
+    // REVALIDATE SERVER PAGES
+    revalidatePath("/");    
+    // revalidatePath("/about");  agre or kahi bhi use ho rha hoga too page ko yaha per import karnege mere isme abhi home page per ho rha hai
+    // revalidatePath("/portfolio"); 
 
     return NextResponse.json({
       success: true,
