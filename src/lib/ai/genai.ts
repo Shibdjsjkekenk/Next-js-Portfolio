4c0bb23338148321c18fdd3b08f8c5263d3226e0
shubh <tiwarishubhanshu22499@gmail.com>
2026-02-28 14:30:27 +0530

    updated genai file

--- src/lib/ai/genai.ts
+++ src/lib/ai/genai.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const apiKey = process.env.GROQ_API_KEY!;
 
-// production me ye log hata dena
+// production me ye log hata denaa
 // console.log("GROQ KEY START:", apiKey?.slice(0, 10));
 
 export const ai = new OpenAI({
