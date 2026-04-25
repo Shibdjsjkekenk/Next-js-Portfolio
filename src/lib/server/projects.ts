181abbb6a45089fd81b7c5108894f84f363467fa
shubh <tiwarishubhanshu22499@gmail.com>
2026-04-25 14:07:46 +0530

    updated project file in lib server

--- src/lib/server/projects.ts
+++ src/lib/server/projects.ts
@@ -1,40 +1,19 @@
-export const dynamic = "force-dynamic";
-
 import { connectDB } from "@/lib/db";
 import Project from "@/models/Project";
-import redis from "@/lib/redis";
-import { CACHE_KEYS } from "@/lib/cacheKeys";
-import { CACHE_TTL } from "@/lib/cacheTTL";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function getActiveProjects() {
+  noStore(); // disable Next.js cache
+
   try {
-    // Use ACTIVE cache key (IMPORTANT)
-    const cached = await redis.get(CACHE_KEYS.PROJECT_ACTIVE);
-
-    if (cached) {
-      // console.log("REDIS ACTIVE HIT");
-      return JSON.parse(cached);
-    }
-
-    console.log("DB ACTIVE HIT");
-
     await connectDB();
 
     const projects = await Project.find({ isActive: true })
       .sort({ order: 1, createdAt: -1 })
       .lean();
 
-    if (!projects.length) return [];
+    return projects || [];
 
-    // Cache ACTIVE projects separately
-    await redis.set(
-      CACHE_KEYS.PROJECT_ACTIVE,
-      JSON.stringify(projects),
-      "EX",
-      CACHE_TTL.MEDIUM
-    );
-
-    return projects;
   } catch (e) {
     console.error("Active projects fetch failed", e);
     return [];
