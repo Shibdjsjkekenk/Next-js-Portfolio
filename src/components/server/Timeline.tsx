faefee96c9da9653741e6ee7d48cfb7544389be5
shubh <tiwarishubhanshu22499@gmail.com>
2026-04-25 13:31:41 +0530

    updated timeline file in server

--- src/components/server/Timeline.tsx
+++ src/components/server/Timeline.tsx
@@ -1,10 +1,11 @@
+export const dynamic = "force-dynamic";
+
 import TimelineClient from "@/components/client-view/TimelineSection";
 import { getActiveTimelines } from "@/lib/server/timeline";
 
 export default async function Timeline() {
   const timelines = await getActiveTimelines();
 
-  //  MAKE JSON SAFE (same as Hero)
   const safeTimelines = timelines
     ? JSON.parse(JSON.stringify(timelines))
     : [];
